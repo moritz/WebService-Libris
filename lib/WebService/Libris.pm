16e5e21e0a41ecb42eff2d2657d031b2a22576a3
Moritz Lenz <moritz@faui2k3.org>
2011-06-15 11:57:01 +0200

    flesh out enough to make simple requests, and obtain the title of a book

--- lib/WebService/Libris.pm
+++ lib/WebService/Libris.pm
@@ -1,5 +1,6 @@
 package WebService::Libris;
 use Mojo::Base -base;
+use Mojo::UserAgent;
 
 use 5.010;
 use strict;
@@ -14,7 +15,8 @@ my %default_typemap = (
 );
 
 has 'id';
-has 'what';
+has 'type';
+has '_dom';
 
 has 'type_map';
 
@@ -33,19 +35,26 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use WebService::Libris;
 
     my $book = WebService::Libris->new(
-        what => 'book',
+        type => 'book',
         # Libris ID
         id   => '9604288',
     );
     print $book->title;
 
+=head1 DESCRIPTION
+
+The Swedish public libraries and the national library of Sweden have a common
+catalogue containing meta data of the books they have available.
+
+This includes many contemporary as well as historical books.
+
+The catalogue is available online at L<http://libris.kb.se>, and can be
+queried with a public API.
+
+This module is a wrapper around this API, and uses the RDF responses to extract data from.
 
 =head1 METHODS
 
@@ -55,18 +64,22 @@ Perhaps a little code snippet.
 
 sub new {
     my ($class, %opts) = @_;;
+    my $c;
     if ($opts{type}) {
+        warn "Type: '$opts{type}'";
         if ($opts{type_map}) {
-            $class = $opts{type_map}{$opts{type}}
-                // $default_typemap{$opts{type}};
+            $c = $opts{type_map}{lc $opts{type}}
+                // $default_typemap{lc $opts{type}};
         } else {
-            $class = $default_typemap{$opts{type}};
+            $c = $default_typemap{lc $opts{type}};
         }
     }
-    if ($class) {
-        return bless \%opts;
+    if ($c) {
+        $class = __PACKAGE__ . "::" . $c;
+        eval "use $class; 1" or die $@;
+        return bless \%opts, $class;
     } else {
-        die "Don't know what do with type '$opts{type}'\n";
+        return bless \%opts, $class;
     }
 }
 
@@ -80,6 +93,25 @@ sub rdf_url {
     my $self = shift;
     my ($key, $id) = $self->fragments;
     "http://libris.kb.se/data/$key/$id?format=application%2Frdf%2Bxml";
+}
+
+=head2 dom
+
+Returns the L<Mojo::DOM> object from the web services response.
+Does a request to the web service if no DOM was stored previously
+
+=cut
+
+sub dom {
+    my $self = shift;
+
+    # TODO: add caching options
+    $self->_dom(Mojo::UserAgent->new()->get($self->rdf_url)->res->dom) unless $self->_dom;
+    $self->_dom;
+}
+
+sub fragments {
+    die "Must be overridden in subclasses";
 }
 
 =head1 AUTHOR
@@ -134,7 +166,7 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
 by the Free Software Foundation; or the Artistic License.
 
-See http://dev.perl.org/licenses/ for more information.
+See L<http://dev.perl.org/licenses/> for more information.
 
 =cut
 
--- lib/WebService/Libris/Book.pm
+++ lib/WebService/Libris/Book.pm
@@ -1,4 +1,16 @@
-package WebService::Libris;
+package WebService::Libris::Book;
+use Mojo::Base 'WebService::Libris';
 use strict;
 use warnings;
 use 5.010;
+
+sub fragments {
+    'bib', shift->id;
+}
+
+sub title { shift->dom->at('title' )->text }
+sub isbn  { shift->dom->at('isbn10')->text }
+sub date  { shift->dom->at('date'  )->text }
+
+
+1;
