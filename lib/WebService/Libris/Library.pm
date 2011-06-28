d6b02d6f26f47714eae47c5ee84815c866ee0cb1
Moritz Lenz <moritz@faui2k3.org>
2011-06-28 11:11:56 +0200

    document Library.pm

--- lib/WebService/Libris/Library.pm
+++ lib/WebService/Libris/Library.pm
@@ -10,5 +10,46 @@ sub fragments {
     'library', shift->id;
 }
 
+=head1 NAME
+
+WebService::Libris::Library - swedish libraries
+
+=head1 SYNOSPIS
+
+    use 5.010;
+    use WebService::Libris;
+    my $book = (WebService::Libris->search(terms => 'Astrid Lindgren'))[0];
+    for my $library ($book->held_by) {
+        say $library->name;
+    }
+
+=head1 DESCRIPTION
+
+Objects of this class represent libraries. 
+
+WebService::Libris::Library inherits all methods from from L<WebService::Libris>.
+
+=head1 METHODS
+
+All of the following methods return a string on succes, and undef if the
+information is not available.
+
+=head2 name
+
+Name of the library
+
+=head2 lat
+
+Lattiude of the physical location of this library
+
+=head2 long
+
+Longitude of the physical location of this library
+
+=head2 homepage
+
+URL to this librarie's homepage
+
+=cut
 
 1;
